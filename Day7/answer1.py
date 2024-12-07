dcafbb8d116d6c97900fdd0bb0622c325aea3002
kevin harris <20600822+kcharris@users.noreply.github.com>
2024-12-07 07:36:15 -0600

    Add Day 7 answer

--- Day7/answer1.py
+++ Day7/answer1.py
@@ -1,1 +1,29 @@
 f = open("Day7\data.txt")
+res = 0
+arr = []
+for line in f.readlines():
+    line = line.rstrip()
+    nums = list(line.split())
+    nums[0] = nums[0][:len(nums[0])-1]
+    nums = list(map(int, nums))
+    arr.append(nums)
+
+def helper(i, total, target, nums):
+    if i == len(nums):
+        if total == target:
+            return True
+        else:
+            return False
+    res = False
+    res |= helper(i+1, total + nums[i], target, nums)
+    res |= helper(i+1, total * nums[i], target, nums)
+    return res
+
+for i in range(len(arr)):
+    if helper(2, arr[i][1], arr[i][0], arr[i]):
+        res += arr[i][0]
+
+print(res)
+        
+
+    
