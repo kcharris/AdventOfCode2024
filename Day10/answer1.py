cfb9dcc80fd9f677c3ffd630578d446aafdfc48b
kevin harris <kcharris77@gmail.com>
2024-12-10 02:47:22 -0600

    Update fn name

--- Day10/answer1.py
+++ Day10/answer1.py
@@ -12,7 +12,7 @@ m = len(arr)
 n = len(arr[0])
 d = ((1, 0),(-1,0),(0,1),(0,-1))
 
-def bfs(t):
+def dfs(t):
     y, x = t
     if arr[y][x] == 9:
         return 1
@@ -22,13 +22,13 @@ def bfs(t):
         ny = y + yd
         nx = x + xd
         if (ny < m and ny >= 0 and nx < n and nx >= 0 and arr[y][x] + 1 == arr[ny][nx] and (ny, nx) not in v):
-            res += bfs((ny,nx))
+            res += dfs((ny,nx))
     return res
 
 res = 0
 for i in range(m):
     for j in range(n):
         if arr[i][j] == 0:
-            res += bfs((i, j))
+            res += dfs((i, j))
 
 print(res)
